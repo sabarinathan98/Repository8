98bb2aa3f8039545068da3161dfe367573149b12
sabarinathan98 <sabarinathan4028@gmail.com>
2021-07-21 11:11:41 +0530

    sabari

---
+++ Method.java
@@ -0,0 +1,15 @@
+package org.ol.ol;
+
+public class Method {
+	public static void add()
+	{
+		System.out.println("q");
+	}
+	public static void main(String[] args) 
+	{
+		Method m=new Method();
+		m.add();
+		
+
+}
+}
